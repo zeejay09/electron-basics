a202f9b7335f45e357504a59383448210ec01a97
zeejay09 <zeejaybelamide@gmail.com>
2020-09-28 20:11:34 +0800

    adds child window and make it a modal with auto show and auto close

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const {app, BrowserWindow} = require('electron')
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-let mainWindow
+let mainWindow, secondaryWindow
 
 // Create a new BrowserWindow when `app` is ready
 function createWindow () {
@@ -12,11 +12,28 @@ function createWindow () {
     width: 1000, height: 800,
     webPreferences: { nodeIntegration: true },
     // show: false
-    backgroundColor: '#2B2E3B'
+    // backgroundColor: '#2B2E3B'
+  })
+
+  secondaryWindow = new BrowserWindow({
+    width: 600, height: 300,
+    webPreferences: { nodeIntegration: true },
+    parent: mainWindow,
+    modal: true,
+    show: false
   })
 
   // Load index.html into the new BrowserWindow
   mainWindow.loadFile('index.html')
+  secondaryWindow.loadFile('secondary.html')
+
+  setTimeout(() => {
+    secondaryWindow.show()
+    setTimeout(() => {
+      secondaryWindow.close()
+      secondaryWindow = null
+    }, 3000)
+  }, 2000)
 
   // Open DevTools - Remove for PRODUCTION!
   // mainWindow.webContents.openDevTools();
@@ -27,6 +44,10 @@ function createWindow () {
   mainWindow.on('closed',  () => {
     mainWindow = null
   })
+
+  // secondaryWindow.on('closed',  () => {
+  //   secondaryWindow = null
+  // })
 }
 
 app.on('browser-window-blur', e => {
